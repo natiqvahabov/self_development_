ac8e3a34e84374d520c8985da1354f699fa5e4cb
natiqvahabov <natiqvahabov@gmail.com>
2019-05-03 17:00:15 +0400

    Python Data Analysis first commit

---
+++ Python_Data_Analysis_2nd-Oreilly/analyze.py
@@ -0,0 +1,17 @@
+'''
+Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991
+'''
+
+#Pandas initial release is 2008, Jan
+
+import pandas as pd
+
+'''
+As Python is an interpreted programming language, in general most Python code will run substantially slower than code written in a compiled language like Java or C++. As programmer time is often more valuable than CPU time, many are happy to make this trade-off
+'''
+
+'''
+Compiled Language: C, C++, Objective C
+Interpreted Language: Php, JS
+Hybrid: Python, Java, VB
+'''
